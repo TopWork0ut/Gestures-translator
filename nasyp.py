86abf5eef2020374ec07b77797d94ae5740325e6
TopWork0ut <olegiksen@gmail.com>
2021-10-30 20:43:54 +0300

    added file nasyp.py with camera capturing

---
+++ nasyp.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+# Виводження фотки
+# img = cv.imread('Photos/1.jpg')
+
+# cv.imshow('name',img)
+
+# cv.waitKey(0)
+
+
+
+
+
+# Виводження відео
+
+# capture = cv.VideoCapture("Videos/випускний кліп школа №98.mp4")
+capture = cv.VideoCapture(0)
+
+while True:
+	isTrue, frame = capture.read()
+	cv.imshow('Video',frame)
+
+	if cv.waitKey(20) & 0xFF==ord('d'):
+		break
+
+capture.release()
+cv.destroyALLWindows()		
+
